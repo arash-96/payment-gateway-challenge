6e154f6439e4f3159019681bf53e3ca6c7341cb2
Arash Sanaiha <arash.sanaiha@yourdoctorfm.com>
2024-05-26 15:41:55 +0100

    unit test for PostPayment controller

--- test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
+++ test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
@@ -1,9 +1,14 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 
+using Newtonsoft.Json;
+
 using PaymentGateway.Api.Controllers;
+using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Models.Responses;
 using PaymentGateway.Api.Services;
 
@@ -12,7 +17,7 @@ namespace PaymentGateway.Api.Tests;
 public class PaymentsControllerTests
 {
     private readonly Random _random = new();
-    
+
     [Fact]
     public async Task RetrievesAPaymentSuccessfully()
     {
@@ -58,4 +63,63 @@ public class PaymentsControllerTests
         // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
+
+    [Fact]
+    public async Task PostPayment_ReturnError()
+    {
+        // Arrange
+        var postPaymentRequest = new PostPaymentRequest // Valid object
+        {
+            CardNumber = "2222405343248112", //valid
+            ExpiryMonth = _random.Next(13, 20), // invalid
+            ExpiryYear = _random.Next(DateTime.Now.Year - 20, DateTime.Now.Year - 10), // invalid
+            Currency = "GBP", //valid
+            Amount = _random.Next(1, 90000), //valid
+            Cvv = _random.Next(100, 1000) //valid
+        };
+
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        var client = webApplicationFactory.CreateClient();
+
+        var jsonContent = JsonConvert.SerializeObject(postPaymentRequest);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await client.PostAsync("/api/Payments", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    public async Task PostPayment_ReturnOk()
+    {
+        // Arrange
+        var postPaymentRequest = new PostPaymentRequest // Valid object
+        {
+            CardNumber = "2222405343248112", //valid
+            ExpiryMonth = _random.Next(1, 13), //valid
+            ExpiryYear = _random.Next(DateTime.Now.Year - 20, DateTime.Now.Year - 10), // invalid
+            Currency = "GBP", //valid
+            Amount = _random.Next(1, 90000), //valid
+            Cvv = _random.Next(100, 1000) //valid
+        };
+
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        var client = webApplicationFactory.CreateClient();
+
+        var jsonContent = JsonConvert.SerializeObject(postPaymentRequest);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await client.PostAsync("/api/Payments", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentResponse);
+    }
+
 }
